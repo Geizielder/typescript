a62a59065090134e733dddd1b6e7d12fb0898e3d
Geizielder <geizielder.gervazio@gmail.com>
2023-01-31 22:04:36 -0300

    add decorators escape

--- typescript-curso-1-arquivos-iniciais/app/src/decorators/inspact.ts
+++ typescript-curso-1-arquivos-iniciais/app/src/decorators/inspact.ts
@@ -4,7 +4,7 @@
     descriptor: PropertyDescriptor
   ) {
     const metodoOriginal = descriptor.value;
-    descriptor.value =function (...args: any[]) {
+    descriptor.value = function (...args: any[]) {
       console.log(`----Método ${propertyKey}`);
       console.log(`-------Parâmetros: ${JSON.stringify(args)}`);
       const retorno = metodoOriginal.apply(this, args);
