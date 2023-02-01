7f9594952476471faa6c9706532a0b59cc6c24ec
Geizielder <geizielder.gervazio@gmail.com>
2023-02-01 17:09:58 -0300

    add dom-injector.ts

--- typescript-curso-1-arquivos-iniciais/app/src/views/view.ts
+++ typescript-curso-1-arquivos-iniciais/app/src/views/view.ts
@@ -14,8 +14,6 @@ export abstract class View<T> {
         }
     }
 
-    @logarTempoDeExecucao(true)
-    @inspact
     public update(model: T): void {
         let template = this.template(model);
         this.elemento.innerHTML = template;
